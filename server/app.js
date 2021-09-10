dd93e45775f02b5e304f2ebacd59716bda92d29a
decadence127 <decadence127@gmail.com>
2021-09-10 20:22:16 +0300

    added production setup

--- server/app.js
+++ server/app.js
@@ -2,12 +2,22 @@ const express = require("express");
 require("dotenv").config();
 const Employee = require("./entities/entities");
 let dbStart = require("./database");
+const path = require("path");
 
 const app = express();
 
 app.use(express.json({ extended: true }));
 
 app.use("/api/office", require("./routes/officeRoute"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static(path.join(__dirname, "client", "build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const start = async () => {
   try {
     await dbStart();
